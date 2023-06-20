2f8d8f164ab0c7f7d99d17ab1d792ee55cba688f
Nate Oster <nateoster@gmail.com>
2023-06-19 21:25:02 -0400

    activate fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'offline';
+const CACHE_NAME = 'offline-v1';
 const OFFLINE_URL = 'index.html';
 const TO_CACHE = [
   "/favicon.ico",
@@ -27,18 +27,33 @@ self.addEventListener('install', function(event) {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('[ServiceWorker] Activate');
-  event.waitUntil((async () => {
-    // Enable navigation preload if it's supported.
-    // See https://developers.google.com/web/updates/2017/02/navigation-preload
-    if ('navigationPreload' in self.registration) {
-      await self.registration.navigationPreload.enable();
-    }
-  })());
+// self.addEventListener('activate', (event) => {
+//   console.log('[ServiceWorker] Activate');
+//   event.waitUntil((async () => {
+//     // Enable navigation preload if it's supported.
+//     // See https://developers.google.com/web/updates/2017/02/navigation-preload
+//     if ('navigationPreload' in self.registration) {
+//       await self.registration.navigationPreload.enable();
+//     }
+//   })());
 
-  // Tell the active service worker to take control of the page immediately.
-  self.clients.claim();
+//   // Tell the active service worker to take control of the page immediately.
+//   self.clients.claim();
+// });
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key === CACHE_NAME) {
+            return;
+          }
+          return caches.delete(key);
+        })
+      );
+    })
+  );
 });
 
 // self.addEventListener('fetch', function(event) {
